6060eea145338496c41b707fa814508c7d7133e5
Vladislav <vvintsyun@gmail.com>
2024-08-03 13:24:15 +0200

    logging removed

--- TestAPITask/Program.cs
+++ TestAPITask/Program.cs
@@ -14,22 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpLogging(logging => {
-    logging.LoggingFields = HttpLoggingFields.Duration 
-        | HttpLoggingFields.RequestQuery
-        | HttpLoggingFields.RequestPath
-        | HttpLoggingFields.RequestMethod
-        | HttpLoggingFields.ResponsePropertiesAndHeaders
-        | HttpLoggingFields.ResponseBody;
-});
-
 var app = builder.Build();
-
-//app.UseHttpLogging();
-app.MapWhen(
-    context => context.Request.Path.StartsWithSegments("/api/"),
-    builder => builder.UseHttpLogging()
-);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
